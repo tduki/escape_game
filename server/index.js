c0cdbb67c656bfd29840f42d71826de71fd645e5
tduki <95369488+tduki@users.noreply.github.com>
2025-10-06 15:04:10 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,13 @@ import cors from 'cors';
 import helmet from 'helmet';
 import { GameManager } from './gameLogic.js';
 import swaggerUi from 'swagger-ui-express';
-import swaggerSpec from './swagger.json' assert { type: 'json' };
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const swaggerSpec = JSON.parse(fs.readFileSync(path.join(__dirname, 'swagger.json'), 'utf-8'));
 
 const app = express();
 const httpServer = createServer(app);
